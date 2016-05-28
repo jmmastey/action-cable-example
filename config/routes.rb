9cd9c350b2a36232fb5770d42c4cd35e42a9de8f
Joseph Mastey <jmmastey@gmail.com>
2016-05-28 11:35:34 -0500

    bookkeeping

--- config/routes.rb
+++ config/routes.rb
@@ -12,7 +12,7 @@ Rails.application.routes.draw do
 
   resources :chatrooms, param: :slug
   resources :messages
-  
+
   # Serve websocket cable requests in-process
   mount ActionCable.server => '/cable'
 end
